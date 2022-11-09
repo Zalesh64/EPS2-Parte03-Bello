ae694a5960f6466a97b18c497c8ebf84cf1646ee
6Manuel9 <1813011488@untels.edu.pe>
2022-11-09 09:47:21 -0500

    Archivo creado por Quijahuaman

---
+++ Aplicacion.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+def menu():
+    print( "\n1. Registrar" )
+    print( "2. Eliminar" )
+    print( "3. Editar" )
+    print( "4. Listar" )
+    print( "5. Salir" )
+
+menu()
