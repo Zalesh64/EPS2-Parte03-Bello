452beb1220fa5d157cfcbee37356fc3924847d89
Zalesh64 <Leafsoul2399@gmail.com>
2022-11-09 11:01:18 -0500

    archivo desarrollado por bello

--- Aplicacion.py
+++ Aplicacion.py
@@ -5,13 +5,36 @@ Spyder Editor
 This is a temporary script file.
 """
 import sqlite3
-
+numero = 0
 def menu():
     print( "\n1. Registrar" )
     print( "2. Eliminar" )
     print( "3. Editar" )
     print( "4. Listar" )
     print( "5. Salir" )
+
+def listar():
+    print("\n** LISTAR PRODUCTOS ** \n")
+    consulta = """ SELECT * 
+                FROM producto
+                """
+                
+    cursor =conexion.cursor()        
+    cursor.execute(consulta)
+    proc = cursor.fetchall()    
+    
+    for producto in proc:
+        print(producto)
+
+    
+
+menu()
+opcion = input("escoga una accion: ")
+if (opcion ==4):
+    print("489465468")
+        
+
+
 
 
 conexion = sqlite3.connect("Apellidos_almacen.db")
@@ -31,4 +54,3 @@ cursor.execute(tabla_producto)
 
 conexion.close()
 
-menu()
