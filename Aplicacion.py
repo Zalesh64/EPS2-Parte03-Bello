d5a1855c960780211bbfb276c8bb1bfef162b29b
Henry Carrasco <1913010968@untels.edu.pe>
2022-11-09 10:13:50 -0500

    Desarrollado por carrasco

--- Aplicacion.py
+++ Aplicacion.py
@@ -4,6 +4,7 @@ Spyder Editor
 
 This is a temporary script file.
 """
+import sqlite3
 
 def menu():
     print( "\n1. Registrar" )
@@ -11,5 +12,23 @@ def menu():
     print( "3. Editar" )
     print( "4. Listar" )
     print( "5. Salir" )
+
+
+conexion = sqlite3.connect("Apellidos_almacen.db")
+
+tabla_producto =""" CREATE TABLE producto(
+                idproducto INTEGER PRIMARY KEY AUTOINCREMENT,
+                codigo INTEGER,
+                nombre TEXT UNIQUE,
+                precio REAL
+                )
+            """
+
+
+        
+cursor = conexion.cursor()
+cursor.execute(tabla_producto)
+
+conexion.close()
 
 menu()
